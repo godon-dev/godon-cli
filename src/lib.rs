1a48d28fda5eb9bcba17de12fc7d8ff987523015
Matthias Tafelmeier <matthias.tafelmeier.cherusk@gmail.com>
2026-03-19 09:23:39 +0100

    Revert "fix: accept uuid alias for id in Breeder response"
    This reverts commit 007fb86c7098c782e6393edbb1a0b99f09932fa0.

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,6 @@ pub struct BreederSummary {
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Breeder {
-    #[serde(alias = "uuid")]
     pub id: String,
     pub name: String,
     pub status: String,
